f8e73f21a8530e42c285d3828465ce9b6d04cebd
shdboom <928421021@qq.com>
2022-06-10 17:43:27 +0800

    底部代码优化

--- components/Footer/index.vue
+++ components/Footer/index.vue
@@ -3,11 +3,11 @@
     <div class="content">
       <div class="footer-item flex justify-between mg-15-auto">
         <div class="w-3/12">
-          <img class="w-40" src="https://qiniu.easyapi.com/market/logo.svg">
-          <p class="w-4/5 mt-6 text-xl">
+          <img class="w-24" src="https://qiniu.easyapi.com/market/logo.svg">
+          <p class="w-4/5 mt-6 text-xs">
             新一代API接口管理平台，EasyAPI是一个为您提供API管理、API测试、API监控、API文档、API低代码服务的综合性API服务平台
           </p>
-          <p>联系电话：<span class="color-00AEC5">136-5617-1020</span></p>
+          <p class="text-xs">联系电话：<span class="color-00AEC5">136-5617-1020</span></p>
           <div class="flex">
             <el-popover
               placement="top"
@@ -24,49 +24,49 @@
         <div class="w-6/12 flex justify-around">
           <div class="flex flex-col">
             <h2>产品介绍</h2>
-            <a href="/product/doc">API协作</a>
-            <a href="/product/lowcode">API低代码</a>
-            <a href="/product/test">API测试</a>
-            <a href="/product/interface">API服务</a>
-            <a href="/product/monitor">API监控</a>
-            <a href="/product/scene">API场景化服务</a>
+            <a class="text-xs" href="/product/doc">API协作</a>
+            <a class="text-xs" href="/product/lowcode">API低代码</a>
+            <a class="text-xs" href="/product/test">API测试</a>
+            <a class="text-xs" href="/product/interface">API服务</a>
+            <a class="text-xs" href="/product/monitor">API监控</a>
+            <a class="text-xs" href="/product/scene">API场景化服务</a>
           </div>
           <div class="flex flex-col">
             <h2>私有化方案</h2>
-            <a href="/solution/portal">API管理门户</a>
-            <a href="/solution/market">API服务市场</a>
-            <a href="/solution/open">API开放平台</a>
+            <a class="text-xs" href="/solution/portal">API管理门户</a>
+            <a class="text-xs" href="/solution/market">API服务市场</a>
+            <a class="text-xs" href="/solution/open">API开放平台</a>
           </div>
           <div class="flex flex-col">
             <h2>使用文档</h2>
-            <a href="https://help.easyapi.com" target="_blank">帮助中心</a>
-            <a href="https://support.qq.com/products/352231" target="_blank">BUG反馈</a>
-            <a href="/info/extension">浏览器插件</a>
-            <a href="/info/github">前端开源计划</a>
+            <a class="text-xs" href="https://help.easyapi.com" target="_blank">帮助中心</a>
+            <a class="text-xs" href="https://support.qq.com/products/352231" target="_blank">BUG反馈</a>
+            <a class="text-xs" href="/info/extension">浏览器插件</a>
+            <a class="text-xs" href="/info/github">前端开源计划</a>
           </div>
           <div class="flex flex-col">
             <h2>关于</h2>
-            <a href="/info/about">关于我们</a>
-            <a href="/info/together">合作伙伴</a>
-            <a href="/info/contact">联系我们</a>
-            <a href="/info/support">支持我们</a>
-            <a href="/post/">平台动态</a>
+            <a class="text-xs" href="/info/about">关于我们</a>
+            <a class="text-xs" href="/info/together">合作伙伴</a>
+            <a class="text-xs" href="/info/contact">联系我们</a>
+            <a class="text-xs" href="/info/support">支持我们</a>
+            <a class="text-xs" href="/post/">平台动态</a>
           </div>
         </div>
         <div class="w-2/12 flex justify-end text-center">
           <div class="mr-12">
             <img class="w-32" src="https://qiniu.easyapi.com/qrcode/leida.png" alt="EasyAPI技术咨询">
-            <p class="mt-5">技术咨询</p>
+            <p class="mt-5 text-xs">技术咨询</p>
           </div>
           <div>
             <img class="w-32" src="https://qiniu.easyapi.com/qrcode/suri.png" alt="EasyAPI商务咨询">
-            <p class="mt-5">商务咨询</p>
+            <p class="mt-5 text-xs">商务咨询</p>
           </div>
         </div>
       </div>
       <el-divider class="mg-15-auto"></el-divider>
       <div class="mg-15-auto text-center">
-        <a href="https://beian.miit.gov.cn/" target="_blank">Copyright © 2015～2022 无锡帮趣数据服务有限公司</a>
+        <a class="text-xs" href="https://beian.miit.gov.cn/" target="_blank">Copyright © 2015～2022 无锡帮趣数据服务有限公司</a>
       </div>
     </div>
   </div>
@@ -77,42 +77,3 @@
 
   export default Index
 </script>
-<style lang="scss">
-  .footer {
-    background: #3C3C3C;
-    color: gray;
-    padding: 20px 0;
-    font-size: 1.2rem;
-  }
-
-  h2 {
-    font-size: 16px;
-    margin-bottom: 10px;
-  }
-
-  p {
-    margin-bottom: 10px;
-  }
-
-  a {
-    color: gray;
-    margin-bottom: 10px;
-  }
-
-  a:hover {
-    color: #fff;
-  }
-
-  .mg-15-auto {
-    margin: 15px auto;
-  }
-
-  .color-00AEC5 {
-    color: #00AEC5;
-  }
-
-  .el-divider {
-    background-color: #606266;
-  }
-
-</style>
