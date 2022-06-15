499c52a3c53f2ebc36de4327e4361e566d72b005
shdboom <928421021@qq.com>
2022-06-15 15:05:53 +0800

    monitor首页优化

--- pages/monitor/index.js
+++ pages/monitor/index.js
@@ -1,14 +1,18 @@
 import './index.scss'
 
 export default {
-  name: 'InfoGitHub',
-  head() {
-    return {
-      title: '前端开源 - EasyAPI服务平台',
-      meta: [
-        {hid: 'description', name: 'description', content: 'EasyAPI前端开源计划'},
-        {hid: 'keyword', name: 'keyword', content: '前端开源'}
-      ]
-    }
+  layout: 'header',
+  head: {
+    title: '帮助中心-API监控-监控方式',
+    meta: [
+      {
+        hid: '帮助中心-API监控-监控方式',
+        name: '帮助中心-API监控-监控方式',
+        content: '帮助中心-API监控-监控方式'
+      }
+    ]
   },
+  data() {
+    return {}
+  }
 }
--- pages/monitor/index.vue
+++ pages/monitor/index.vue
@@ -1,34 +1,36 @@
 <template>
   <div>
-    <div class="mon-content">
-      <div class="mon-title">监控方式</div>
-      <div class="mon-img">
-        <div class="mon-img-li">
-          <img src="/images/monitor/camera.png"/>
+    <div class="mon-content content h-full block mx-auto py-12">
+      <div class="mon-title text-2xl">监控方式</div>
+      <el-divider></el-divider>
+      <div class="flex">
+        <div class="text-center mr-8">
+          <img class="mb-4" src="/images/monitor/camera.png"/>
           <p>自定义监控</p>
         </div>
-        <div class="mon-img-li">
-          <img src="/images/monitor/code.png"/>
+        <div class="text-center mr-8">
+          <img class="mb-4" src="/images/monitor/code.png"/>
           <p>代码监控</p>
         </div>
-        <div class="mon-img-li">
-          <img src="/images/monitor/sdk.png"/>
+        <div class="text-center mr-8">
+          <img class="mb-4" src="/images/monitor/sdk.png"/>
           <p>SDK监控</p>
         </div>
       </div>
-      <div class="mon-con-title">1、SDK方式</div>
-      <div class="mon-con-content">
+      <el-divider></el-divider>
+      <div class="text-xl mt-12">1、SDK方式</div>
+      <div class="ml-9 text-base mt-6 w-2/4 leading-10">
         如果您觉得SDK监控集成比较复杂，那么我们提供了一套轻量级的代码监控方式，您只需要在您的Android与IOS代码中嵌入一端简单的代码即可。
       </div>
-      <div class="mon-con-title">2、代码监控</div>
-      <div class="mon-con-content">
+      <div class="text-xl mt-12">2、代码监控</div>
+      <div class="ml-9 text-base mt-6 w-2/4 leading-10">
         如果您觉得SDK监控集成比较复杂，那么我们提供了一套轻量级的代码监控方式，您只需要在您的Android与IOS代码中嵌入一端简单的代码即可。
         <p>
           如图：
         </p>
       </div>
-      <div class="mon-con-title">1、自定义监控</div>
-      <div class="mon-con-content">
+      <div class="text-xl mt-12">1、自定义监控</div>
+      <div class="ml-9 text-base mt-6 w-2/4 leading-10">
         如果您觉得SDK监控集成比较复杂，那么我们提供了一套轻量级的代码监控方式，您只需要在您的Android与IOS代码中嵌入一端简单的代码即可。
         <p>
           如图：
@@ -37,75 +39,8 @@
     </div>
   </div>
 </template>
-
 <script>
-export default {
-  layout: 'header',
-  head: {
-    title: '帮助中心-API监控-监控方式',
-    meta: [
-      {
-        hid: '帮助中心-API监控-监控方式',
-        name: '帮助中心-API监控-监控方式',
-        content: '帮助中心-API监控-监控方式'
-      }
-    ]
-  },
-  data() {
-    return {}
-  }
-}
+  import Index from './index'
 
+  export default Index
 </script>
-
-<style scoped lang="stylus" rel="stylesheet/stylus">
-.mon-con-title
-  font-size 16px
-  font-weight 800px
-  color #000
-  width 600px
-  min-height 10px
-  margin-top 30px
-
-.mon-con-content
-  margin-left 20px
-  margin-top 15px
-  width 600px
-  min-height 10px
-  line-height 25px
-
-
-.mon-content
-  width 960px
-  height 100%
-  display block
-  margin 0 auto
-  padding 30px 0
-
-  .mon-title
-    width 100%
-    font-size 16px
-    padding-bottom 20px
-    border-bottom 1px solid #c1c1c1
-
-  .mon-img
-    width 100%
-    min-width 10px
-    display flex
-    margin-top 10px
-
-    .mon-img-li
-      flex 1
-      display flex
-      flex-direction column
-      align-items center
-      text-align center
-
-      p
-        margin-top 10px
-
-      img
-        width 250px
-        height 130px
-
-</style>
