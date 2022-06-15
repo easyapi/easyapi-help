6ed5e9d6c8901578b66fb5a5d67e4db2096f33ca
shdboom <928421021@qq.com>
2022-06-15 16:03:27 +0800

    sdk页面优化

---
+++ pages/monitor/sdk/index.js
@@ -0,0 +1,18 @@
+import './index.scss'
+
+export default {
+  layout: 'header',
+  head: {
+    title: '帮助中心-API监控-SDK下载',
+    meta: [
+      {
+        hid: '帮助中心-API监控-SDK下载',
+        name: '帮助中心-API监控-SDK下载',
+        content: '帮助中心-API监控-SDK下载'
+      }
+    ]
+  },
+  data() {
+    return {}
+  }
+}
---
+++ pages/monitor/sdk/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <!--<div class="header">-->
+    <!--<div class="min-header">-->
+    <!--更新历史-->
+    <!--</div>-->
+    <!--</div>-->
+    <div class="content h-full block mx-auto">
+      <div class="my-8"><img src="/images/monitor/android.png" alt=""/></div>
+      <div class="text-2xl flex items-center justify-start mb-8">
+        <div class="content-btn">Android SDK 下载</div>
+        2016-10-21 V2.1.5(2016-06-03)
+      </div>
+      <ol type="1" class="text-xl">
+        <li>1、优化Native异常合并；</li>
+        <li>2、优化上报流量控制</li>
+        <li>3、优化运营数据逻辑</li>
+        <li>4、修复小概率数据重复上报问题</li>
+      </ol>
+      <a class="block mt-8 text-xl mb-16" href="">
+        Android SDK包 更新历史
+      </a>
+      <div class="content-title"><img src="/images/monitor/ios.png" alt=""/></div>
+      <div class="text-2xl flex items-center justify-start mb-8">
+        <div class="content-btn">IOS SDK 下载</div>
+        2016-10-21 V2.1.5(2016-06-03)
+      </div>
+      <ol type="1" class="text-xl">
+        <li>1、优化Native异常合并；</li>
+        <li>2、优化上报流量控制</li>
+        <li>3、优化运营数据逻辑</li>
+        <li>4、修复小概率数据重复上报问题</li>
+      </ol>
+      <a class="block mt-8 text-xl mb-16" href="">
+        IOS SDK包 更新历史
+      </a>
+    </div>
+  </div>
+</template>
+<script>
+  import Index from './index'
+
+  export default Index
+</script>
