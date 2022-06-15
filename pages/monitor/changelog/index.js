0870478b9f71e452ac3a95949ef770f48e8df4f5
shdboom <928421021@qq.com>
2022-06-15 16:15:01 +0800

    changelog页面优化

---
+++ pages/monitor/changelog/index.js
@@ -0,0 +1,19 @@
+import './index.scss'
+
+export default {
+  layout: 'header',
+  head: {
+    title: '帮助中心-API监控-更新历史',
+    meta: [
+      {
+        hid: '帮助中心-API监控-更新历史',
+        name: '帮助中心-API监控-更新历史',
+        content: '帮助中心-API监控-更新历史'
+      }
+    ]
+  },
+  data() {
+    return {}
+  }
+}
+
