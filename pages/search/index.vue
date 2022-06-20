5063d66dd4b748d5abaf418eae522bc8d77905e5
shdboom <928421021@qq.com>
2022-06-20 17:14:15 +0800

    搜索页面优化

--- pages/search/index.vue
+++ pages/search/index.vue
@@ -5,13 +5,13 @@
       <div class="mb-12">共找到 {{ size }} 条 「任务」的搜索结果</div>
       <div class="con py-4" v-for="item in content" v-if="!item.video">
         <div class="text-2xl mb-8">{{ item.title }}</div>
-        <div class="text-lg">{{ item.content }}</div>
+        <div class="text-lg searchText" v-html="item.content"></div>
       </div>
     </div>
   </div>
 </template>
 <script>
-import Index from './index'
+  import Index from './index'
 
-export default Index
+  export default Index
 </script>
